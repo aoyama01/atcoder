3ea274ea661dd1f17a513728f7dead664e43a7b2
aoyama01 <139197283+aoyama01@users.noreply.github.com>
2025-05-19 17:55:39 +0900

    refactor EDPC c

--- EDPC/c.py
+++ EDPC/c.py
@@ -1,30 +1,20 @@
 vacation_day_num = int(input())
 
-happiness_by_swimming = []
-happiness_by_insect = []
-happiness_by_homework = []
-
-for i in range(vacation_day_num):
+# happiness[i][k] = i日目に行動k(0:水泳, 1:虫取り, 2:宿題)をしたときの幸福度
+happiness = []
+for _ in range(vacation_day_num):
     a, b, c = map(int, input().split())
-    happiness_by_swimming.append(a)
-    happiness_by_insect.append(b)
-    happiness_by_homework.append(c)
+    happiness.append([a, b, c])
 
-# 最初の i 日間での最適解
+# dp[i][j] = i日目までで、前日に行動jをしたときの最大幸福度
 dp = [[0] * 3 for _ in range(vacation_day_num + 1)]
 
 for i in range(vacation_day_num):
-    for j in range(3):
-        for k in range(3):
+    for j in range(3):  # 前日の行動
+        for k in range(3):  # 今日の行動
             if j == k:
                 continue
-            if k == 0:
-                dp[i + 1][k] = max(dp[i + 1][k], dp[i][j] + happiness_by_swimming[i])
-            if k == 1:
-                dp[i + 1][k] = max(dp[i + 1][k], dp[i][j] + happiness_by_insect[i])
-            if k == 2:
-                dp[i + 1][k] = max(dp[i + 1][k], dp[i][j] + happiness_by_homework[i])
-
+            dp[i + 1][k] = max(dp[i + 1][k], dp[i][j] + happiness[i][k])
 
 ans = max(dp[vacation_day_num])
 print(ans)
