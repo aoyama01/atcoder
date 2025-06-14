9fc5f8e77a02597dc6c3b8c15e39d8c4b61cb751
aoyama01 <139197283+aoyama01@users.noreply.github.com>
2025-06-14 23:44:09 +0900

    ABC410 a

---
+++ ABC/401-450/410/a.py
@@ -0,0 +1,12 @@
+# https://atcoder.jp/contests/abc410/tasks
+
+n = int(input())
+a = list(map(int, input().split()))
+k = int(input())
+
+cnt = 0
+for i in range(n):
+    if a[i] >= k:
+        cnt += 1
+
+print(cnt)
